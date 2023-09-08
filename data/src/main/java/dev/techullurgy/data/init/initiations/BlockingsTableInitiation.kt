7fddeaf244798ed93efc4d85f0fb66a1171c40f3
Mikkareem <mohdirsath1997@gmail.com>
2023-09-08 19:51:55 +0530

    Small fix

--- data/src/main/java/dev/techullurgy/data/init/initiations/BlockingsTableInitiation.kt
+++ data/src/main/java/dev/techullurgy/data/init/initiations/BlockingsTableInitiation.kt
@@ -3,10 +3,13 @@ package dev.techullurgy.data.init.initiations
 import dev.techullurgy.data.init.schema.BlockingsTable
 import dev.techullurgy.data.init.schema.BlockingsTableStructure
 import dev.techullurgy.data.init.schema.TableStructure
+import org.ktorm.database.Database
 
 internal object BlockingsTableInitiation: TableInitiation {
     override val tableName: String
         get() = BlockingsTable
     override val tableStructure: TableStructure
         get() = BlockingsTableStructure
+
+    override suspend fun initiate(database: Database): Boolean = super.initiate(database)
 }
